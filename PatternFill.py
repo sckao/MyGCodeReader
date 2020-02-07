3801c034d75f64a62955310488d64589165afbd4
sckao <kaoshihchuan@gmail.com>
2020-02-07 17:01:02 -0600

    complete the new pattern fill class, self check inside polygon and connect with boundary

--- PatternFill.py
+++ PatternFill.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 
+# http://www.dcs.gla.ac.uk/~pat/52233/slides/Geometry1x1.pdf
 
 class PatternFill :
 
@@ -11,12 +12,15 @@ class PatternFill :
         self.bs = self.bw*self.bsScale
         self.yScale = 1.
 
-    def setBeadSpace(self, bw, bsscale ):
+    def setBeadSpacing(self, bw, bsscale ):
 
         self.bw = bw
         self.bsScale = bsscale
 
     # providing shapes from left to right
+    # n: number of point in upper/bottom half, radius, dL : spacing
+    # flip : upper or bottom half of the polygon
+    # return points
     def unitPolygon(self, n, r, dL, x0, y0, flip = False ) :
 
         pattern = []
@@ -28,7 +32,7 @@ class PatternFill :
         for i in range(n+2) :
             dx = r*math.cos( theta )
             dy = r*math.sin( theta )
-            x = x0 + r + dx
+            x = x0 + dx
             if flip is True :
                 y = y0 - dy
             else :
@@ -75,10 +79,66 @@ class PatternFill :
 
         top, bott, left, right = self.findRange(pos)
 
-        width = left[0] - right[0]
+        width = right[0] - left[0]
         height = top[1] - bott[1]
 
         return width, height
+
+    # This polygon does not take into account of bead spacing between top half and bottom half
+    def unitPolygonSize(self, n,m,r,dL ):
+
+        u1 = self.unitPolygon( n, r, dL, r, r,  )
+        b1 = self.unitPolygon( m, r, dL, r, r, True )
+        b1.reverse()
+        ub = u1 + b1
+        width, height = self.unitSize( ub )
+        print(' Unit polygon size = %.2f, %.2f' %(width, height))
+
+        return width, height
+
+
+    # BL : bottom-left position, TR : top-right position
+    def createPattern(self, BL, TR, n, m, r, dL  ):
+
+        L = TR[0] - BL[0]
+        H = TR[1] - BL[1]
+
+        w0, h0 = self.unitPolygonSize( n, m, r, dL )
+        h1 = h0 + self.bs
+
+        nX = int(L / w0) + 1
+        nY = int(H*2 / h1) + 1
+        print(' nX = %d , nY = %d' %(nX, nY) )
+
+        x = BL[0] + r
+        y = TR[1] - (h0/2)
+        arcs = []
+        dx = (2*r) + dL
+        dy = h1
+        for j in range( nY ) :
+
+            for i in range( nX ) :
+
+                if j%2 == 0 :
+                    u1 = self.unitPolygon( n, r, dL, x, y )
+                    if i < nX-1 :
+                        del u1[-1]
+                        x = x + dx
+                    arcs = arcs + u1
+                else :
+                    b1 = self.unitPolygon( m, r, dL, x, y, True )
+                    b1.reverse()
+                    arcs = arcs + b1
+                    if i < nX-1 :
+                        x = x - dx
+
+            if j%2 == 0 :
+                y = y - self.bs
+            else :
+                y = y - dy
+
+        return arcs
+
 
     # if val == 0  -> colinear and r is between p and q
     #    val == 2  -> colinear and r is Outside p or q
@@ -134,10 +194,14 @@ class PatternFill :
         return x, y
         # print( '(%d,%d) = [ %.3f, %.3f ]' %( i, j, c[0], c[1]) )
 
+    # return the intersection point
     def doIntersect(self, p1, q1, p2, q2 ):
 
         interlist = []
         # find out the 4 orientation
+        # if val == 0  -> colinear p-r-q
+        #    val == 2  -> colinear p-q-r  or r-p-q
+        #    val == 1  -> clockwise, val == -1 -> counterclockwise
         o1 = self.orientation( p1, q1, p2 )
         o2 = self.orientation( p1, q1, q2 )
         o3 = self.orientation( p2, q2, p1 )
@@ -151,9 +215,10 @@ class PatternFill :
         else :
 
             # special cases , one or two points are colinear
-            # p1, q1, p2 are colinear and p2 is between p1 and q1
+            # p1-p2-q2-q1,
             if o1 == 0 and o2 == 0 :
                 interlist.append( p2 )
+            # p2-p1-q1-q2,
             if o1 == 3 and o4 == 0 :
                 interlist.append( p1 )
 
@@ -166,85 +231,87 @@ class PatternFill :
             if o4 == 0 and o1 != 0 and o2 != 0 and o3 != 0 :
                 interlist.append( q1 )
 
+        return interlist
 
-    def inRangeCheck(self, arc, pos ):
 
+    # arc is the pattern list, pos is the outline list
+    def filterPattern(self, arc, pos ):
+
+        # the element in this container is [x,y, stat]
+        # stat = 0 -> inside polygon, stat = 1 -> outside polygon , given boundary point
         patV = []
+        # loop all pattern point to check whether they are inside the boundary
         for i in range( len(arc) ) :
 
+            itlist = []
+            bList = []
+            # form a horizental line
+            qx = [999, arc[i][1]]
+            p1 = arc[i]
+            q1 = arc[i-1]
             for j in range( len(pos) ) :
 
-                p1 = arc[i]
-                q1 = [999, arc[i][1]]
-
+                # testing outline segment
                 p2 = pos[j-1]
                 q2 = pos[j]
-                itlist = self.doIntersect( p1, q1, p2, q2 )
 
-                # odd number of intersection means the point is inside the polygon
-                if len(itlist)%2 == 1 :
-                    patV.append( arc[i] )
-                else :
+                # check to see if the point is inside the polygon
+                testP = self.doIntersect( p1, qx, p2, q2 )
+                if len(testP) > 0 :
+                    itlist.append( testP )
+                # check if the point across the boundary
+                boundP = self.doIntersect( p1, q1, p2, q2 )
+                if len(boundP) > 0 :
+                    for it in boundP :
+                        bList.append( [ it[0], it[1], j ] )
 
+            # Add the boundary point
+            if len(bList) > 0 :
+                patV.append( [bList[0][0], bList[0][1], 1, bList[0][2] ] )
 
+            # odd number of intersection means the point is inside the polygon
+            if len(itlist)%2 == 1 :
+                patV.append( [arc[i][0], arc[i][1], 0 ] )
 
+        patF = []
+        for i in range( len(patV) ):
 
+            # whatever the 1st point or the inside points are collected
+            if i == 0 or patV[i][2] == 0:
+                patF.append( [patV[i][0], patV[i][1]] )
+                continue
 
+            # If two adjacent boundary points happen, connect them with outline points
+            if patV[i][2] == 1 and patV[i-1][2] == 1 and i == 1:
+                patF.append( [patV[i][0], patV[i][1]] )
+            if patV[i][2] == 1 and patV[i-1][2] == 1 and i > 1:
+                j = patV[i][3]
+                k = patV[i-1][3]
 
+                if abs(j-k+1) < abs(k-j+1) :
+                    for m in range( abs(j-k+1) ) :
+                        patF.append( [ pos[k-m-1][0], pos[k-m-1][1] ] )
 
+                if abs(j-k+1) > abs(k-j+1) :
+                    for m in range( abs(k-j+1) ) :
+                        patF.append( [ pos[k+1+m][0], pos[k+1+m][1] ] )
 
-
-
-
-    def fillSpace(self, pos):
-
-        beadSpacing = self.BSScale * self.beadWidth
-
-        # set up pattern type
-        # n =2 , r =4, dL =4 , (x0,y0) = (0,0)
-        pattern0  = self.unitPolygon(2,4,4,0,0)
-
-        # determine the unit polygon size
-        w0, h0 = self.unitSize( pattern0 )
-        h0 = (h0 * self.yScale) + beadSpacing
-        print('w: %.3f , h: %.3f' % (w0, h0))
-
-        # Find the top,bottom, left and right range
-        top, bott, left, right = self.find(pos)
-        print('Range top: %.2f , bott: %.2f, left: %.2f , right: %.2f' % (top[1], bott[1], left[0], right[0]))
-        # Partition the rectangle space
-        nX = int((right[0] - left[0]) / w0) + 1
-        nY = int((top[1] - bott[1]) / (h0 + beadSpacing)) + 1
-        print('NX: %d , NY: %d' % (nX, nY))
-
-        # Determine starting point
-        x = left[0]
-        y = top[1] - h0
-        flip = False
-        fillV = []
-        for j in range( nY ) :
-
-            # Layout the pattern in the partition range
-            if j%2 == 1 :
-                flip = True
-            for i in range( nX ) :
-                patt_i = self.unitPolygon(2,4,4,x, y, flip)
-                if flip is True :
-                    patt_i.reverse()
-
-                fillV = fillV + patt_i
-                x = x + w0
-
-            # Check the pattern whether they are in the range
-            for k in range( len(pos) ) :
-
-                if pos[k][1] <= y and pos[k][1] > (y+h) :
-                    continue
-
-                x1 = pos[k][0]
-                y1 = pos[k][1]
+        return patF
 
 
+    def fillSpace(self, pos , n, m, r, dL, sx = 0, sy = 0  ):
 
+        top, bott, left, right = self.findRange(pos)
 
-            y = y - h0
+        TR = [ right[0]+sx, top[1]+sy ]
+        BL = [ left[0]+sx, bott[1]+sy ]
+
+        #print( TR )
+        #print( BL )
+
+        arcs = self.createPattern( BL, TR, n, m, r, dL )
+        print( ' len of arcs : %d' %( len(arcs) ) )
+        patt = self.filterPattern( arcs, pos )
+        print( ' len of patt : %d' %( len(patt) ) )
+
+        return patt
